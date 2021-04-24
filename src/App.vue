1ee5bde9867b35f8486c48e923a490fbc4e2397a
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-04-24 12:03:11 -0300

    feat: clean up components and add css

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Pokemon />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Pokemon from './components/Pokemon';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Pokemon
   }
 }
 </script>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Pokemon</h1>
+</template>
+
+<script>
+export default {
+  name: 'Pokemon',
+}
+</script>
+
